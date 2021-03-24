25eaad5a0643db278842f67696db0b1e8e880594
David Everett <davideverett1989@gmail.com>
2021-03-24 00:22:41 -0500

    Routes and basic views in place.

---
+++ src/views/Auth.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Auth">
+      <h1>Auth</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Auth',
+}
+</script>
+
+<style>
+.Auth {
+    display: block;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Home">
+      <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
+
+<style>
+.Home {
+    display: block;
+}
+</style>
